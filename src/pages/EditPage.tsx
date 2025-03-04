e52724a5996a4d3e773cbb7fd0c0ad5dabb77e00
Alex <malych@ukr.net>
2025-03-04 16:49:34 +0200

    EditPage: made validation, status field and redirect after form submitting

--- src/pages/EditPage.tsx
+++ src/pages/EditPage.tsx
@@ -1,4 +1,4 @@
-import {useParams, Link} from 'react-router-dom'
+import {useParams, Link, useNavigate } from 'react-router-dom'
 import {useState, useEffect} from "react";
 import {Container, Row, Col, Button, Form} from 'react-bootstrap';
 
@@ -13,8 +13,10 @@ const EditPage = () => {
     const {id} = useParams();
     const API_URL = 'http://localhost:3000/books';
 
+    const navigate = useNavigate();
+
     const emptyBook: Book =     {
-        "id": 0,
+        "id": '',
         "title": '',
         "category": '',
         "description": '',
@@ -26,6 +28,8 @@ const EditPage = () => {
     }
 
     const [singleBook, setSingleBook] = useState(emptyBook);
+    const [validating, setValidating] = useState<boolean>(false);
+    const [status, setStatus] = useState<string>('');
 
     useEffect(() => {
         if (id) {
@@ -44,15 +48,15 @@ const EditPage = () => {
     const onSubmit = (e: React.FormEvent) => {
         e.preventDefault();
 
+        setValidating(true);
+
         const date = new Date();
 
         const month = date.toLocaleString('default', { month: 'long' }),
             time = date.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true }),
             formattedDate = `${date.getDate()} ${month} ${date.getFullYear()}, ${time}`;
 
-        const lastUpdated = singleBook.created ? formattedDate : 'a';
-
-        console.log('lastUpdated', lastUpdated, formattedDate)
+        const lastUpdated = singleBook.created ? formattedDate : '-';
 
         const bookUpdated = {
             "id": singleBook.id || uuidv4(),
@@ -65,35 +69,45 @@ const EditPage = () => {
             "status": 'active',
         };
 
-        fetch(`${API_URL}/${bookUpdated.id}`)
-            .then((response) => {
-                if (response.status === 200 || response.status === 201) {
-                    return response.json()
-                }
-            })
-            .then((data) => {
-                if (data) {
-                    // Item exists, so update it with PUT request
-                    return fetch(`${API_URL}/${bookUpdated.id}`, {
-                        method: 'PUT',
-                        headers: {
-                            'Content-Type': 'application/json',
-                        },
-                        body: JSON.stringify(
-                            {...bookUpdated }),
-                    });
-                } else {
-                    // Item does not exist, so create it with POST request
-                    return fetch(`${API_URL}`, {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json',
-                        },
-                        body: JSON.stringify(bookUpdated),
-                    });
-                }
-            })
-            .catch((error) => {console.error('Error updating book status on server:', error);});
+        const sendRequestTrue = singleBook.title && singleBook.category && singleBook.ISBN;
+
+        if (sendRequestTrue) {
+            fetch(`${API_URL}/${bookUpdated.id}`)
+                .then((response) => {
+                    if (response.status === 200 || response.status === 201) {
+                        return response.json()
+                    }
+                })
+                .then((data) => {
+                    if (data) {
+                        // Item exists, so update it with PUT request
+                        return fetch(`${API_URL}/${bookUpdated.id}`, {
+                            method: 'PUT',
+                            headers: {
+                                'Content-Type': 'application/json',
+                            },
+                            body: JSON.stringify(
+                                {...bookUpdated }),
+                        });
+                    } else {
+                        // Item does not exist, so create it with POST request
+                        return fetch(`${API_URL}`, {
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json',
+                            },
+                            body: JSON.stringify(bookUpdated),
+                        });
+                    }
+                })
+                .then(() => {
+                    setTimeout(()=> navigate('/'),3000);
+                    setStatus('Done! You will be redirected in 3 seconds to the Homepage');
+                })
+                .catch((error) => {console.error('Error updating book status on server:', error);});
+        } else {
+            setStatus('Please fill out all required fields');
+        }
     }
 
     const onValueChange = (e:  React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
@@ -101,6 +115,12 @@ const EditPage = () => {
             ...singleBook,
             [e.target.name]: e.target.value,
         });
+
+        if (e.target.name && e.target.value) {
+            e.target.classList.remove("border-danger");
+        } else {
+            e.target.classList.add('border-danger');
+        }
     }
 
 
@@ -118,7 +138,7 @@ const EditPage = () => {
                 <Col className='mb-5'>
                     {
                         singleBook?.id ?
-                            <h2>Edit Book with ID {id}</h2>
+                            <h2>Book Editing</h2>
                             : <h2>Add a New Book</h2>
                     }
                 </Col>
@@ -128,37 +148,37 @@ const EditPage = () => {
                 <Col className="">
                     <Form onSubmit={(e) => onSubmit(e)}>
                         <Form.Group className="mb-3" controlId="formBookTitle">
-                            <Form.Label>Book title</Form.Label>
+                            <Form.Label>Book Title</Form.Label>
 
                             <Form.Control
-                                required
                                 type="text"
                                 name="title"
                                 placeholder="Enter a Title"
+                                className={`border ${!title && validating ? 'border-danger' : ''}`}
                                 value={title}
-                                onChange={(e) =>onValueChange(e)}
+                                onChange={(e) => onValueChange(e)}
                             />
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="formBookAuthor">
-                            <Form.Label>Author name</Form.Label>
+                            <Form.Label>Author Name</Form.Label>
 
                             <Form.Control
-                                required
                                 type="text"
                                 name="author"
                                 placeholder="Enter Author"
+                                className={`border ${!author && validating ? 'border-danger' : ''}`}
                                 value={author}
-                                onChange={(e) =>onValueChange(e)}/>
+                                onChange={(e) => onValueChange(e)}/>
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="formBookCategory">
-                            <Form.Label>Category {category}</Form.Label>
+                            <Form.Label>Category</Form.Label>
 
                             <Form.Select
                                 name="category"
                                 value={category}
-                                required
+                                className={`border ${!category && validating ? 'border-danger' : ''}`}
                                 onChange={(e) => onValueChange(e)}>
                                 <option value="" disabled>Choose Category</option>
                                 <option value="Adventure">Adventure</option>
@@ -171,13 +191,17 @@ const EditPage = () => {
                             <Form.Label>International Standard Book Number (ISBN)</Form.Label>
 
                             <Form.Control
-                                required
                                 type="number"
                                 name='ISBN'
                                 placeholder="Enter ISBN"
+                                className={`border ${!ISBN && validating ? 'border-danger' : ''}`}
                                 value={ISBN}
-                                onChange={(e) =>onValueChange(e)}/>
+                                onChange={(e) => onValueChange(e)}/>
                         </Form.Group>
+
+                        <div className="">
+                            <p><b>{status}</b></p>
+                        </div>
 
                         <Button variant="primary" type="submit">
                             {
